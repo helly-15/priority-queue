8e5873cb22ac3b75f5c298522542260d49283efd
Lenocka <lvutina.en@gmail.com>
2019-10-15 23:56:46 +0300

    17 tests pass

--- src/max-heap.js
+++ src/max-heap.js
@@ -2,7 +2,8 @@ const Node = require('./node');
 
 class MaxHeap {
 	constructor() {
-		
+		this.root=null;
+		this.parentNodes=[];
 	}
 
 	push(data, priority) {
@@ -34,7 +35,7 @@ class MaxHeap {
 	}
 
 	insertNode(node) {
-		
+		return this.root=node;
 	}
 
 	shiftNodeUp(node) {
--- src/node.js
+++ src/node.js
@@ -9,18 +9,46 @@ class Node {
 	}
 
 	appendChild(node) {
+		if ( this.right==null){
+			if(this.left){
+				this.right=node;
+				this.right.parent=this;
+				return this.right
+			}
+
+			this.left=node;
+			this.left.parent=this;
+			return 	this.left;
+		}
 		
+
 	}
 
 	removeChild(node) {
+		if (node==this.left){ 
+			this.left.parent=null;
+			this.left=null
+		}
+		else if (node==this.right){ 
+			this.right.parent=null;
+			this.right=null
+		}
+		else throw "Error"
+		
 
 	}
 
 	remove() {
+		
 
 	}
 
 	swapWithParent() {
+		if(this.parent!=null){
+			return this.parent=this.left;
+
+		
+		}
 		
 	}
 }
