e3a3c775deda716a6266fb5556bf4809edef613e
Lenocka <lvutina.en@gmail.com>
2019-10-15 23:18:08 +0300

    9 tests pass

--- src/node.js
+++ src/node.js
@@ -1,10 +1,15 @@
 class Node {
 	constructor(data, priority) {
+		this.data=data;
+		this.priority=priority;
+		this.parent=null;
+		this.left=null;
+		this.right=null;
 
 	}
 
 	appendChild(node) {
-
+		
 	}
 
 	removeChild(node) {
